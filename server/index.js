89d7916e2ca2ae8719e2794f03e3590d63383d68
colin <colinbruzas@gmail.com>
2022-02-09 20:45:13 -0700

    dots3

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -14,4 +15,8 @@ app.get("/api", (req, res) => {
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
